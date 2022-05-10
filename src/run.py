4441519329a72bdcc4c308f218317c29261b283d
pvhil <54856838+pvhil@users.noreply.github.com>
2022-05-10 19:06:11 +0200

    Release: Forgot a header

--- src/run.py
+++ src/run.py
@@ -67,6 +67,7 @@ class warehousemonitor():
                 "sec-fetch-user": "?1",
                 "upgrade-insecure-requests": "1",
                 "viewport-width": "1920",
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36"
             }
             try:
                 if proxy == "null":
